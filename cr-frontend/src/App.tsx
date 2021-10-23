9d9c25ebe5b928f467763b699bebe5691e28f2cb
carrynong2 <carrynong2@gmail.com>
2021-10-23 10:19:10 +0700

    extraced CourseItem component

--- cr-frontend/src/App.tsx
+++ cr-frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import CourseItem from "./CourseItem";
+import { Course } from "./interfaces";
 
 const App = () => {
-  const [courses, setCourses] = useState<any[]>([]);
+  const [courses, setCourses] = useState<Course[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/courses")
@@ -16,9 +18,7 @@ const App = () => {
     <div className="App">
       <ul>
         {courses.map((item) => (
-          <li key={item.id}>
-            {item.number} - {item.title}
-          </li>
+          <CourseItem key={item.id} course={item} />
         ))}
       </ul>
     </div>
---
+++ cr-frontend/src/CourseItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Course } from "./interfaces";
+
+type CourseItemProps = {
+  course: Course;
+};
+
+const CourseItem = (props: CourseItemProps) => {
+  const course: Course = props.course;
+  return (
+    <li className="Course">
+      {course.number} - {course.title}
+    </li>
+  );
+};
+
+export default CourseItem;
