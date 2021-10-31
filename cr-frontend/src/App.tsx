61f313848ad20fb1bdf9c614aa4e0f23351e9b9e
carrynong2 <carrynong2@gmail.com>
2021-10-31 09:29:37 +0700

    uses React Router Dom

--- cr-frontend/src/App.tsx
+++ cr-frontend/src/App.tsx
@@ -1,15 +1,41 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import LoginForm from "./components/LoginForm";
 import About from "./components/About";
 import CourseReview from "./components/CourseReview";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <CourseReview />
-      <LoginForm />
-      <About />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login">
+            <LoginForm />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <CourseReview />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
